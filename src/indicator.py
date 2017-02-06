3404ed38a4f1aa6a4983b179d5033bc131e1c833
TerCZ <t.chuzhe@qq.com>
2017-02-06 16:01:56 +0800

    添加五个标记后的表盘图像，现可批量处理此类图像（定位、剪裁、找出指针），待添加角度处理部分。

--- src/indicator.py
+++ src/indicator.py
@@ -5,12 +5,79 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
-FILENAME = "../pic/cut1.png"
-
-
-def find_lines(filename, test=True):
+def cut_panel(img_filename, template_filename, test=True):
     """
-    读取图像，将一定角度范围的线条找到并返回其相关参数
+    读取图像、十字标记模版，通过模版匹配定位表盘，并剪裁待处理区域
+    :param img_filename: 图像文件名
+    :param template_filename: 模版文件名
+    :param test: 测试模式
+    :return: 剪裁后的表盘图像
+    """
+    # 读取图像
+    img = cv2.imread(img_filename)
+    # 转换为灰度图像
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # 读取十字标记模版
+    template = cv2.imread(template_filename, 0)
+    w, h = template.shape[::-1]
+
+    # 模版匹配
+    res = cv2.matchTemplate(gray, template, cv2.TM_CCOEFF_NORMED)
+    # 使用阈值处理过滤结果
+    threshold = 0.90
+    loc = np.where(res >= threshold)
+    # 记录十字的四个顶点
+    pts1 = []
+    for pt in zip(*loc[::-1]):
+        # 计算十字中心
+        cross_x, cross_y = pt
+        cross_x += w / 2
+        cross_y += h / 2
+        # 判断是否重复选择此点
+        to_add = True
+        for x, y in pts1:
+            if abs(x - cross_x) <= 10 and abs(y - cross_y) <= 10:
+                to_add = False
+        if not to_add:
+            continue
+        # 记录此点
+        pts1.append([cross_x, cross_y])
+        # 测试时画图可视化
+        if test:
+            cv2.rectangle(img, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
+    # 测试时画图可视化
+    if test:
+        cv2.imshow("wa", img), cv2.waitKey(), cv2.destroyAllWindows()
+
+    # 确定表盘的中心
+    ratio = 0.65    # 剪裁比例
+    x1 = pts1[0][0]
+    x2 = pts1[1][0]
+    y1 = pts1[0][1]
+    y2 = pts1[3][1]
+    delta_x = x2 - x1
+    delta_y = y2 - y1
+    x1 = x1 + (1 - ratio) / 2 * delta_x
+    x2 = x2 - (1 - ratio) / 2 * delta_x
+    y1 = y1 + (1 - ratio) / 2 * delta_y
+    y2 = y2 - (1 - ratio) / 2 * delta_y
+    # 确定透视变换对应的点
+    pts1 = np.float32([[x1, y1], [x2, y1], [x2, y2], [x1, y2]])
+    pts2 = np.float32([[0, 0], [200, 0], [200, 200], [0, 200]])
+    # 透视变换
+    M = cv2.getPerspectiveTransform(pts1, pts2)
+    img = cv2.warpPerspective(img, M, (200, 200))
+
+    # 测试时画图可视化
+    if test:
+        cv2.imshow("wa", img), cv2.waitKey(), cv2.destroyAllWindows()
+
+    return img
+
+
+def find_lines(img, test=True):
+    """
+    从仪表图像，将一定角度范围的线条找到并返回其相关参数
     :param filename: 图像地址
     :param test: 是否可视化调参
     :return: 由线(rho, theta)为元素的列表
@@ -26,9 +93,6 @@ def find_lines(filename, test=True):
     hough_thrs = np.arange(50, 100, 10)    # Hough线检测阈值调参范围
     hough_thr = 60  # Hough线检测阈值
     angle_epsilon = 10  # 线的角度范围允许误差
-
-    # 读取图像
-    img = cv2.imread(filename)  # 读为黑白
 
     # 测试时将调参过程可视化
     if test:
@@ -103,11 +167,18 @@ def find_angle(lines):
     :return: 指针的角度
     """
 
-    pass
+    return 0.0
 
 
 def main():
-    find_lines(FILENAME, test=True)
+    files = ["../pic/uncut1.png", "../pic/uncut2.png", "../pic/uncut3.png", "../pic/uncut4.png", "../pic/uncut5.png"]
+    template = "../pic/cross.png"
+
+    for filename in files:
+        img = cut_panel(filename, template, test=True)
+        lines = find_lines(img, test=True)
+        angle = find_angle(lines)
+        print u"示数为:", angle
 
 
 if __name__ == '__main__':
